a9836c14e046eac4a3360531e506c84699386ffc
KaoriAkagi <149767729+KaoriAkagi@users.noreply.github.com>
2025-07-05 16:37:56 +0900

    Add test files 3

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -6,6 +6,6 @@ import "testing"
 func TestEvenOrOdd(t *testing.T) {
     result := EvenOrOdd(10)
     if result != "even" {
-        t.Errorf("expected: even, actual: %$", result)
+        t.Errorf("expected: even, actual: %s", result)
     }
 }
